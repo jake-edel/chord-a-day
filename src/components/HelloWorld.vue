a5b2eca7446ebf09883f75858f0683ad50343c30
Jakob Edelstein <jakobedelstein@gmail.com>
2023-02-18 18:40:16 -0700

    adds vue boilerplate

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      I just propped up a
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a> app.
+      We're about to pop off and create <del>chord-a-day</del> scale-a-day.
+      Get ready to be friends.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
